5bec656fd88dae6256c8ba14e72b5ec294523e00
andres-benavides <andre0190@gmail.com>
2019-06-06 20:39:19 -0500

    Guardar el rin y los puntos por rin y perfil

--- routes/api.php
+++ routes/api.php
@@ -15,49 +15,56 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\AccessTokenController@issueToken');
 Route::get("perro", 'API\TestController@dog');
 //RUTAS PARA EL ADMINISTRADOR
-Route::group(['middleware'=>['auth:api']], function() {
-  
+Route::group(['middleware' => ['auth:api']], function() {
+
   Route::get("allprofiles", 'API\ProfilesController@allProfiles');
   //CRUD PARA LOS USUARIOS
   Route::get("users/all", 'API\UsersController@all');
   Route::post("users/create", 'API\UsersController@create');
   Route::get("users/getuser/{id}", 'API\UsersController@get');
   Route::post("users/update/{id}", 'API\UsersController@update');
-  
+
   //PRODUCTOS
   Route::get("producs/all", 'API\ProductsController@all');
   Route::post("producs/create", 'API\ProductsController@create');
   Route::get("producs/get/{id}", 'API\ProductsController@get');
   Route::post("producs/update/{id}", 'API\ProductsController@update');
-  
+
   //CIUDADES
   Route::get("cities/all", 'API\CitiesController@all');
   Route::post("cities/create", 'API\CitiesController@create');
   Route::get("cities/get/{id}", 'API\CitiesController@get');
   Route::post("cities/update/{id}", 'API\CitiesController@update');
-  
-  
+
+
   //SUCURSALES
   Route::get("subsidiary/all", 'API\SubsidiariesController@all');
   Route::post("subsidiary/create", 'API\SubsidiariesController@create');
   Route::get("subsidiary/get/{id}", 'API\SubsidiariesController@get');
   Route::post("subsidiary/update/{id}", 'API\SubsidiariesController@update');
   Route::post("subsidiary/delete/{id}", 'API\SubsidiariesController@delete');
-  
+
   //MARCAS
   Route::get("brand/all", 'API\BrandController@all');
   Route::post("brand/create", 'API\BrandController@create');
   Route::get("brand/get/{id}", 'API\BrandController@get');
   Route::post("brand/update/{id}", 'API\BrandController@update');
   Route::post("brand/delete/{id}", 'API\BrandController@delete');
-  
-  
+
+
   //DISEÑOS
   Route::get("design/all", 'API\DesignController@all');
   Route::post("design/create", 'API\DesignController@create');
   Route::get("design/get/{id}", 'API\DesignController@get');
   Route::post("design/update/{id}", 'API\DesignController@update');
   Route::post("design/delete/{id}", 'API\DesignController@delete');
+
+  //RINES
+  Route::get("rin/all", 'API\RinController@all');
+  Route::post("rin/create", 'API\RinController@create');
+  Route::get("rin/get/{id}", 'API\RinController@get');
+  Route::post("rin/update/{id}", 'API\RinController@update');
+  Route::post("rin/delete/{id}", 'API\RinController@delete');
 });
 
 
