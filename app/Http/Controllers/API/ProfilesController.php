c478779e25d34a8814c392d7e865909a7939a3a6
Miguel <miguelcamargo9@gmail.com>
2019-07-04 16:21:00 -0500

    Create CRUD for Profiles

--- app/Http/Controllers/API/ProfilesController.php
+++ app/Http/Controllers/API/ProfilesController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
+use Illuminate\Http\Request;
 use App\Profiles;
 
 class ProfilesController extends BaseController {
@@ -18,6 +17,37 @@ class ProfilesController extends BaseController {
     $profiles = Profiles::all();
     return $profiles;
   }
+
+  //Get all profiles
+  public function all() {
+    $profiles = Profiles::all();
+    return $profiles;
+  }
+
+  //Create a new profile
+  public function create(Request $r) {
+    $profileR = json_decode($r->getContent(), true);
+    $profile = new Profiles();
+    foreach ($profileR as $column => $value) {
+      $profile->$column = $value;
+    }
+    return( $profile->save()) ? ["message" => "success"] : ["message" => "error"];
+  }
+
+  //Get an existing profile
+  public function get($id) {
+    $profile = Profiles::find($id);
+    return $profile;
+  }
+
+  //Update a profile
+  public function update($id, Request $r) {
+    $profileR = json_decode($r->getContent(), true);
+    $profile = Profiles::find($id);
+    foreach ($profileR as $column => $value) {
+      $profile->$column = $value;
+    }
+    return( $profile->update()) ? ["message" => "success"] : ["message" => "error"];
+  }
+
 }
-
-
