381677a1e5c14cd792c4cf54432db6a7b99c74e9
Miguel <miguelcamargo9@gmail.com>
2019-07-04 16:21:44 -0500

    Create CRUD for Product Categories

---
+++ app/Http/Controllers/API/ProductCategoriesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use App\ProductCategories;
+
+class ProductCategoriesController extends BaseController {
+
+  use AuthorizesRequests,
+      ValidatesRequests;
+
+  //Get all Product Categories
+  public function all() {
+    $productCategories = ProductCategories::all();
+    return $productCategories;
+  }
+
+  //Create a new Product Category
+  public function create(Request $r) {
+    $productCategoryR = json_decode($r->getContent(), true);
+    $productCategory = new ProductCategories();
+    foreach ($productCategoryR as $column => $value) {
+      $productCategory->$column = $value;
+    }
+    return( $productCategory->save()) ? ["message" => "success"] : ["message" => "error"];
+  }
+
+  //Get an existing Product Category
+  public function get($id) {
+    $productCategory = ProductCategories::find($id);
+    return $productCategory;
+  }
+
+  //Update a Product Category
+  public function update($id, Request $r) {
+    $productCategoryR = json_decode($r->getContent(), true);
+    $productCategory = ProductCategories::find($id);
+    foreach ($productCategoryR as $column => $value) {
+      $productCategory->$column = $value;
+    }
+    return( $productCategory->update()) ? ["message" => "success"] : ["message" => "error"];
+  }
+
+}
