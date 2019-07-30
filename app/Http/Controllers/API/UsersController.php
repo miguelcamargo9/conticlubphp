ebaa4faf211459e102648409281fa4e4f5be6ad1
andres-benavides <andre0190@gmail.com>
2019-07-29 19:57:53 -0500

    emvio cambio de contraseña

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -145,7 +145,7 @@ class UsersController extends BaseController {
   //RECUPERAR UNA CONTRASEÑA
   public function recover(Request $r) {
       $data  = json_decode($r->getContent(), true);
-      $user = User::where([["email"=>$data['email']],["identification_number"=>$data['identification_number']]])->get();
+      $user = User::where([["email",$data['email']],["identification_number",$data['identification_number']]])->get();
       $exitUser = User::where([["email"=>$data['email']],["identification_number"=>$data['identification_number']]])->count();
       if($exitUser>0){
             try {
